f23553d55ab029f11bce426755c763fab2d7eb98
Anton Iskov <iskov.anton@yandex.com>
2016-07-24 21:14:51 +0200

    Add install script

--- mac/README.md
+++ mac/README.md
@@ -1,3 +1,4 @@
 Installation:
 
     ln -s ~/Development/aiconf/mac/.bash_profile .bash_profile
+    ~/Development/aiconf/mac/install.sh
---
+++ mac/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Command line tools
+brew install git
+brew install curl
+brew install wget
+brew install icdiff
+brew install ffmpeg
+brew install p7zip
+brew install HTTPie
+
+brew install ansible
+
+# Install sdkman
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk version
+
+# Install java tools
+sdk install scala
+sdk install groovy
+sdk install maven
+sdk install sbt
+sdk install gradle
