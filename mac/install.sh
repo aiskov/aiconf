1e14e854d5aeaf15c64695e2288f79ee46a0a3f4
Anton Iškov <iskov.anton@yandex.com>
2017-11-04 13:12:12 +0100

    Update install.sh

--- mac/install.sh
+++ mac/install.sh
@@ -36,7 +36,7 @@ brew cask install java
 brew cask install intellij-idea
 brew cask install postman
 brew cask install atom
-brew cask install sublime-text
+brew cask install visual-studio-code
 brew cask install android-studio
 brew cask install docker
 brew cask install kitematic
