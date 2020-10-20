7e3600da105d0f172753f7281218563ab7f027b6
Anton Iškov <iskov.anton@yandex.com>
2020-10-20 10:12:38 +0200

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-apt-get update 
-apt-get upgrade -y
-apt-get install -y git
+sudo apt-get update 
+sudo apt-get upgrade -y
+sudo apt-get install -y git zsh python3-venv htop
+
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+sdk install java 15.0.0-zulu
+sdk install java 11.0.8-zulu
+sdk install java 8.0.265-zulu
+sdk install gradle
 
 git clone git@github.com:aiskov/aiconf.git
 
-mv .bashrc .bashrc_old
+mv ~/.bashrc ~/.bashrc_old
 ln -s aiconf/ubuntu/bashrc.sh .bashrc
+
+chsh -s $(which zsh)
